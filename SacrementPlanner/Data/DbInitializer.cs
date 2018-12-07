6fb1b8785387c47b270aaa35768f7dc137572ff1
minzoe <min16009@byui.edu>
2018-12-06 20:46:54 -0700

    Database Created and Populated

--- SacrementPlanner/Data/DbInitializer.cs
+++ SacrementPlanner/Data/DbInitializer.cs
@@ -32,13 +32,19 @@ namespace SacrementPlanner.Models
 
             var planner = new Planner[]
             {
-                new Planner{OpeningSong="",SacramentSong="",IntermediateSong="",ClosingSong="", Conductor=1, SacramentDate=DateTime.Parse("2009-05-06")},
-                new Planner{OpeningSong="",SacramentSong="",IntermediateSong="",ClosingSong="", Conductor=2, SacramentDate=DateTime.Parse("2009-06-08")},
-                new Planner{OpeningSong="",SacramentSong="",IntermediateSong="",ClosingSong="", Conductor=3, SacramentDate=DateTime.Parse("2009-11-25")},
-                new Planner{OpeningSong="",SacramentSong="",IntermediateSong="",ClosingSong="", Conductor=4, SacramentDate=DateTime.Parse("2012-01-15")},
-                new Planner{OpeningSong="",SacramentSong="",IntermediateSong="",ClosingSong="", Conductor=5, SacramentDate=DateTime.Parse("2018-07-12")},
-                new Planner{OpeningSong="",SacramentSong="",IntermediateSong="",ClosingSong="", Conductor=1, SacramentDate=DateTime.Parse("2016-06-02")}
+                new Planner{OpeningSong="Sing a Song of Sixpence",SacramentSong="There is a Green Hill Far Away",IntermediateSong="Book of Mormon Stories",ClosingSong="Christmas Bells", Conductor=1, SacramentDate=DateTime.Parse("2009-05-06")},
+                new Planner{OpeningSong="Mary had a Little Lamb",SacramentSong="Article of Faith 1",IntermediateSong="A Child's Prayer",ClosingSong="Go Tell it on the Mountain", Conductor=2, SacramentDate=DateTime.Parse("2009-06-08")},
+                new Planner{OpeningSong="Hickory Dickory Dock",SacramentSong="Article of Faith 2",IntermediateSong="",ClosingSong="Silent Night", Conductor=3, SacramentDate=DateTime.Parse("2009-11-25")},
+                new Planner{OpeningSong="Little Miss Muffet",SacramentSong="Article of Faith 4",IntermediateSong="Pioneer Children",ClosingSong="Joy to the World", Conductor=4, SacramentDate=DateTime.Parse("2012-01-15")},
+                new Planner{OpeningSong="Hey Diddle Diddle",SacramentSong="Article of Faith 5",IntermediateSong="All Over the World",ClosingSong="Picture a Christmas", Conductor=5, SacramentDate=DateTime.Parse("2018-07-12")},
+                new Planner{OpeningSong="Funky Jesus Music",SacramentSong="Article of Faith 6",IntermediateSong="",ClosingSong="Said Samuel", Conductor=1, SacramentDate=DateTime.Parse("2016-06-02")}
             };
+
+            foreach (Planner p in planner)
+            {
+                context.Planner.Add(p);
+            }
+            context.SaveChanges();
 
             var speakers = new Speakers[]
             {
@@ -54,6 +60,12 @@ namespace SacrementPlanner.Models
                 new Speakers{Name="Steve Ancho", Topic="Spiritual Health", PlannerId=5},
                 new Speakers{Name="Nathan Jimbob", Topic="Divine Nature", PlannerId=5},
             };
+
+            foreach (Speakers s in speakers)
+            {
+                context.Speakers.Add(s);
+            }
+            context.SaveChanges();
 
         }
     }
---
+++ SacrementPlanner/Migrations/20181207034220_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacrementPlanner.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bishoprics",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bishoprics", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planner",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OpeningSong = table.Column<string>(nullable: true),
+                    SacramentSong = table.Column<string>(nullable: true),
+                    IntermediateSong = table.Column<string>(nullable: true),
+                    ClosingSong = table.Column<string>(nullable: true),
+                    SacramentDate = table.Column<DateTime>(nullable: false),
+                    Conductor = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planner", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Topic = table.Column<string>(nullable: true),
+                    PlannerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bishoprics");
+
+            migrationBuilder.DropTable(
+                name: "Planner");
+
+            migrationBuilder.DropTable(
+                name: "Speakers");
+        }
+    }
+}
